4a5a3f16837f8446506bcd1a2248403ff373a8d7
El Yaouti_Aya <aya.elyaouti@icloud.com>
2026-03-02 14:58:17 +0100

    Mise à jour de pca.py avec code PCA complet v2

--- pca.py
+++ pca.py
@@ -6,21 +6,21 @@ from sklearn.preprocessing import StandardScaler
 
 def main():
 
-    # 1️⃣ Charger le dataset
+    #  Charger le dataset
     df = pd.read_csv("/Users/ayaelyaouti/Documents/ProjetFinalTP/ProjetFinal_Note/city_lifestyle_dataset.csv")
 
-    # 2️⃣ Supprimer les colonnes non numériques
+    #  Supprimer les colonnes non numériques
     X = df.drop(columns=["city_name", "country"])
 
-    # 3️⃣ Standardiser les données (TRÈS IMPORTANT pour PCA)
+    #  Standardiser les données 
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
 
-    # 4️⃣ Appliquer PCA
+    #  Appliquer PCA
     pca = PCA(n_components=2)
     X_pca = pca.fit_transform(X_scaled)
 
-    # 5️⃣ Visualisation
+    #  Visualisation
     plt.figure(figsize=(8,6))
     plt.scatter(X_pca[:,0], X_pca[:,1], alpha=0.7)
     plt.title("Projection 2D des villes avec PCA")
@@ -28,10 +28,10 @@ def main():
     plt.ylabel("PC2")
     plt.show()
 
-    # 6️⃣ Exporter les données 2D
+    #  Exporter les données 2D
     np.savetxt("X_pca_2D.csv", X_pca, delimiter=",")
 
-    # 7️⃣ Observation simple
+    #  Observation simple
     print("Variance expliquée :", pca.explained_variance_ratio_)
 
 if __name__ == "__main__":
